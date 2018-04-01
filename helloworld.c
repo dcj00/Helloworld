086b6d9473026d7652bb094f1698593e32110866
dcj00 <30423445+dcj00@users.noreply.github.com>
2018-04-01 19:34:40 +0800

    add-changes
    I  want to add something new!

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main (void)
 {
-	printf("Hello world!");
+	int a;
+	printf("Hello world!\n");
+	printf("please input an integer!\n");
+	scanf("%d",&a);
+	printf("The input integer is :%d\n",a);
+	printf("Bye!");
 	return 0;
 }
